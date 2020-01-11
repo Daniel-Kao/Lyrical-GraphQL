d4294188f54a75fa2167a895bd2e8c65997f7569
Daniel-Kao <396007476@qq.com>
2020-01-11 21:17:14 +0800

    init

---
+++ client/components/SongList.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { Link } from "react-router";
+import query from "../queries/fetchSongs";
+import gql from "graphql-tag";
+
+class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } }).then(() => {
+      console.log(123);
+    });
+  }
+  renderList() {
+    return this.props.data.songs.map(({ id, title }) => (
+      <li key={id} className="collection-item">
+        {title}
+        <i className="material-icons" onClick={() => this.onSongDelete(id)}>
+          delete
+        </i>
+      </li>
+    ));
+  }
+  render() {
+    console.log(this.props);
+    if (this.props.data.loading) return <div>loading...</div>;
+    return (
+      <div>
+        <ul className="collection">{this.renderList()}</ul>
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(graphql(query)(SongList));
